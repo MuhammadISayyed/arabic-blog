878d7fb34c216e588c3da440b8195a5f9fd30f97
Muhammad I. Sayyed <muhammadisayyed@gmail.com>
2023-09-08 01:52:18 +0300

    Remove boilerplate

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'My Arabic Blog',
+  description: 'Blginnn',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
